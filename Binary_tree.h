d38d33601eeaf5711bbd2bc94d339f1c63741a01
Jow-star <joycefollygan@gmail.com>
2025-12-05 16:11:52 +0100

    Update Binary_tree.h

--- Binary_tree.h
+++ Binary_tree.h
@@ -1,23 +1,50 @@
-#ifndef BINARY_TREE_H_INCLUDED
-#define BINARY_TREE_H_INCLUDED
+#pragma once
 #include <vector>
 #include <iostream>
-#include <iomanip>
+#include <stdexcept>
 
-template<typename T>
+template <typename T>
 class BinaryTree {
 private:
     int _depth;
     std::vector<std::vector<T>> _tree;
 
 public:
-    explicit BinaryTree(int depth = 0);
-    void setDepth(int depth);
-    void setNode(int n, int i, const T& value);
-    T getNode(int n, int i) const;
-    void display() const;
-    int depth() const;
+    // Constructeur
+    BinaryTree() : _depth(0) {}
+
+    // Définit la profondeur et redimensionne les vecteurs
+    void setDepth(int depth) {
+        _depth = depth;
+        _tree.resize(_depth + 1);
+        for (int i = 0; i <= _depth; ++i) {
+            _tree[i].resize(i + 1);
+        }
+    }
+
+    // Setter
+    void setNode(int depth, int index, T value) {
+        if (depth < 0 || depth > _depth || index < 0 || index > depth) {
+            throw std::out_of_range("BinaryTree: Index out of bounds");
+        }
+        _tree[depth][index] = value;
+    }
+
+    // Getter
+    T getNode(int depth, int index) const {
+        if (depth < 0 || depth > _depth || index < 0 || index > depth) {
+            throw std::out_of_range("BinaryTree: Index out of bounds");
+        }
+        return _tree[depth][index];
+    }
+
+    // Affichage
+    void display() const {
+        for (int i = 0; i <= _depth; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                std::cout << _tree[i][j] << "\t";
+            }
+            std::cout << std::endl;
+        }
+    }
 };
-
-
-#endif // PARTIE_2_H_INCLUDED
