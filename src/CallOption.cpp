c3663249c05b081ccd16610839dee1d6ad941a2f
Jow-star <joycefollygan@gmail.com>
2025-12-09 23:09:12 +0100

    Add files via upload

---
+++ src/CallOption.cpp
@@ -0,0 +1,15 @@
+#include "CallOption.h"
+#include <algorithm>
+
+CallOption::CallOption(double expiry, double strike)
+    : EuropeanVanillaOption(expiry, strike) {}
+
+double CallOption::payoff(double z)
+{
+    return std::max(z - getStrike(), 0.0);
+}
+
+EuropeanVanillaOption::optionType CallOption::GetOptionType() const
+{
+    return CALL;
+}
