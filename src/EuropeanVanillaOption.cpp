1e5367c358458e218fa662cdafcc55e04613a875
Jow-star <joycefollygan@gmail.com>
2025-12-09 23:07:00 +0100

    Add files via upload

---
+++ src/EuropeanVanillaOption.cpp
@@ -0,0 +1,17 @@
+#include "EuropeanVanillaOption.h"
+#include <stdexcept>
+
+EuropeanVanillaOption::EuropeanVanillaOption(double expiry, double strike)
+    : Option(expiry), _strike(strike)
+{
+    if (expiry < 0 || strike < 0)
+        throw std::invalid_argument("Expiry et strike doivent être non-négatifs");
+}
+
+
+double EuropeanVanillaOption::getStrike() const
+{
+    return _strike;
+}
+
+
