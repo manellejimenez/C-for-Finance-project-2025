b1b7e3224a3ade4d2f07e0374ac1161a9be886af
Jow-star <joycefollygan@gmail.com>
2025-12-09 23:04:26 +0100

    Add files via upload

---
+++ include/EuropeanVanillaOption.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "EuropeanVanillaOption.h"
+
+class PutOption : public EuropeanVanillaOption
+{
+public:
+    PutOption(double expiry, double strike);
+
+    double payoff(double z) const override;
+    optionType GetOptionType() const override;
+};
