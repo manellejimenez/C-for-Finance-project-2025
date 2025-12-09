1899994305b1c37f52a16cd5658ae43b032002bd
Jow-star <joycefollygan@gmail.com>
2025-12-09 22:49:29 +0100

    Add files via upload

---
+++ include/Option.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Option
+{
+private:
+    double _expiry;
+
+public:
+    Option();
+    Option(double e);
+    virtual ~Option();
+
+    double getExpiry() const;
+
+    virtual double payoff(double z) const = 0;
+
+    // Nouvelle méthode pour la partie 4 (virtuelle simple)
+    virtual bool isAmericanOption();
+
+    // Destructeur virtuel (essentiel pour l'héritage)
+    virtual ~Option();
+};
