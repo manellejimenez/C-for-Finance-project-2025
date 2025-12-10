9bcbbb38eaed024b7583fc9661322e4819d3d232
Jow-star <joycefollygan@gmail.com>
2025-12-10 01:48:42 +0100

    Refactor EuropeanVanillaOption constructor and methods

--- src/EuropeanVanillaOption.cpp
+++ src/EuropeanVanillaOption.cpp
@@ -4,14 +4,14 @@
 EuropeanVanillaOption::EuropeanVanillaOption(double expiry, double strike)
     : Option(expiry), _strike(strike)
 {
-    if (expiry < 0 || strike < 0)
-        throw std::invalid_argument("Expiry et strike doivent être non-négatifs");
+    if (expiry < 0 || strike < 0) {
+        throw std::invalid_argument("Expiry et Strike doivent être positifs ou nuls.");
+    }
 }
 
-
-double EuropeanVanillaOption::getStrike() const
-{
+double EuropeanVanillaOption::getStrike() const {
     return _strike;
 }
+
 
 
