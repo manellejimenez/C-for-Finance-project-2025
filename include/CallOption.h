76eb12b436e33176af346ecb226fc1a71e462301
Jow-star <joycefollygan@gmail.com>
2025-12-10 01:22:16 +0100

    Update CallOption.h

--- include/CallOption.h
+++ include/CallOption.h
@@ -6,6 +6,10 @@ class CallOption : public EuropeanVanillaOption
 public:
     CallOption(double expiry, double strike);
 
-    double payoff(double z) override;
+    double payoff(double z) const override;
+
     optionType GetOptionType() const override;
+
+    virtual ~CallOption() = default;
 };
+
