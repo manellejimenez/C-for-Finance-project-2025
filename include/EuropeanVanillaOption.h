32bd531f9df8ab666b0c50831f5940027bdda3aa
Jow-star <joycefollygan@gmail.com>
2025-12-10 01:21:48 +0100

    Refactor EuropeanVanillaOption class structure
    Refactor EuropeanVanillaOption class and add private member _strike.

--- include/EuropeanVanillaOption.h
+++ include/EuropeanVanillaOption.h
@@ -1,11 +1,27 @@
 #pragma once
-#include "EuropeanVanillaOption.h"
+#include "Option.h"
 
-class PutOption : public EuropeanVanillaOption
+//enum class optionType { CALL, PUT };
+class BlackScholesPricer;
+class EuropeanVanillaOption : public Option
 {
-public:
-    PutOption(double expiry, double strike);
+//BlackScholesPricer as a friend class
+    friend class BlackScholesPricer;
 
-    double payoff(double z) const override;
-    optionType GetOptionType() const override;
+private:
+    double _strike;
+
+public:
+    EuropeanVanillaOption(double expiry, double strike);
+
+    // Getter nécessaire pour les classes filles (car _strike est privé)
+    double getStrike() const;
+
+    // Consigne : "pure virtual method GetOptionType()"
+    virtual optionType GetOptionType() const = 0;
+
+    // Destructeur virtuel par défaut
+    virtual ~EuropeanVanillaOption() = default;
 };
+
+
