06fe6b3356583201fcce92e7a65cfa4fcd75c1ee
Jow-star <joycefollygan@gmail.com>
2025-12-10 01:21:17 +0100

    Implement GetOptionType in digital option classes
    Added GetOptionType method to EuropeanDigitalOption and its derived classes.

--- include/EuropeanDigitalOption.h
+++ include/EuropeanDigitalOption.h
@@ -1,29 +1,44 @@
 #pragma once
 #include "Option.h"
 
+// Déclaration anticipée
+class BlackScholesPricer;
+
+// --- CLASSE MÈRE ---
 class EuropeanDigitalOption : public Option {
+    friend class BlackScholesPricer;
 private:
     double _strike;
 
 public:
     EuropeanDigitalOption(double expiry, double strike);
-
     double getStrike() const;
 
-    // Reste pure virtuelle
-    virtual double payoff(double z) = 0;
+    // Méthode pure virtuelle (abstraite)
+    virtual optionType GetOptionType() const = 0;
 
-    virtual ~EuropeanDigitalOption() {};
+    virtual double payoff(double z) const = 0;
+    virtual ~EuropeanDigitalOption() = default;
 };
 
+// --- CLASSE FILLE : CALL ---
 class EuropeanDigitalCallOption : public EuropeanDigitalOption {
 public:
     EuropeanDigitalCallOption(double expiry, double strike);
-    double payoff(double z) override;
+
+    double payoff(double z) const override;
+
+    // C'EST CETTE LIGNE QUI MANQUAIT :
+    optionType GetOptionType() const override;
 };
 
+// --- CLASSE FILLE : PUT ---
 class EuropeanDigitalPutOption : public EuropeanDigitalOption {
 public:
     EuropeanDigitalPutOption(double expiry, double strike);
-    double payoff(double z) override;
+
+    double payoff(double z) const override;
+
+    // CELLE-CI MANQUAIT AUSSI :
+    optionType GetOptionType() const override;
 };
